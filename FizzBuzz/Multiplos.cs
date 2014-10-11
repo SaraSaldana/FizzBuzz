b385adce9826d6f668c6296ab75a5c455e597cc9
SaraSaldana <sara.saldana@hotmail.com>
2014-10-11 16:55:02 -0500

    Primer Commit

---
+++ FizzBuzz/Multiplos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+   public   class Multiplos
+    {
+       public string  Multiplo( int  num)
+       {
+            var respuesta = "";
+
+           if ( num % 3 == 0 && num % 5== 0)
+          {
+              respuesta += "FizzBuzz";
+              num -= 15;
+          }
+
+         else if (num % 5 == 0)
+            {
+                respuesta += "Buzz";
+                num -= 5;
+            }
+            
+          else if  (num%3==0 )
+                {
+                    respuesta += "Fizz";
+                    num -= 3;
+                }
+          
+          else  
+          {
+              respuesta += num;
+          }
+                
+           
+            return respuesta;
+       }
+    }
+}
---
+++ FizzBuzzTest/FizzBuzzTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FizzBuzz;
+
+namespace FizzBuzzTest
+{
+    [TestFixture]
+     public class FizzBuzzTest
+    {
+        private Multiplos numero { get; set; }
+
+        [SetUp]
+         public void InstanciarAntesDeCadaTest()
+        {
+            //arrange
+            numero = new Multiplos();
+        }
+        [Test]
+        public void Mostrar1()
+        {
+            //arrange
+           
+            //act
+            var multiplo = numero.Multiplo(1);
+
+            //
+            Assert.AreEqual("1", multiplo);
+
+        }
+
+        [Test]
+        public void Mostrar3()
+        {
+            //arrange
+            
+            //act
+            var multiplo = numero.Multiplo(3);
+
+            //
+            Assert.AreEqual("Fizz", multiplo);
+
+        }
+        [Test]
+        public void Mostrar5()
+        {
+            //arrange
+           
+            //act
+            var multiplo = numero.Multiplo(5);
+
+            //
+            Assert.AreEqual("Buzz", multiplo);
+
+        }
+
+        [Test]
+        public void Mostrar15()
+        {
+            //arrange
+            
+            //act
+            var multiplo = numero.Multiplo(15);
+
+            //
+            Assert.AreEqual("FizzBuzz", multiplo);
+
+        }
+        [Test]
+        public void Mostrar6()
+        {
+            //arrange
+            var numero = new Multiplos();
+
+            //act
+            var multiplo = numero.Multiplo(6);
+
+            //
+            Assert.AreEqual("Fizz", multiplo);
+
+        }
+    }
+}
